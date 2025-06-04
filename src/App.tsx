36683243c5e81965913702746af7b9fd825c5ec7
Isa Reinert <reinert.isa@gmail.com>
2025-06-03 18:33:23 -0700

    feat(website): update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import BookList from "./components/book/List.tsx";
+import Navbar from "./components/Navbar.tsx";
+import ThemeContextProvider from "./contexts/ThemeContext.tsx";
+import ThemeToggle from "./components/ThemeToggle.tsx";
+import AuthContextProvider from "./contexts/AuthContext.tsx";
+import BookContextProvider from "./contexts/BookContext.tsx";
+import BookForm from "./components/book/Form.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+          <ThemeContextProvider>
+              <AuthContextProvider>
+                  <Navbar />
+                  <BookContextProvider>
+                      <BookList />
+                      <BookForm />
+                  </BookContextProvider>
+                  <ThemeToggle />
+              </AuthContextProvider>
+          </ThemeContextProvider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
