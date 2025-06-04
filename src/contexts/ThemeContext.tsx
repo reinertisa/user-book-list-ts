9d3e1177822e95d81621eb07c7bb15f2f99c9613
Isa Reinert <reinert.isa@gmail.com>
2025-06-03 18:32:26 -0700

    feat(website): create ThemeContext context

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,55 @@
+import React, {createContext, useState} from 'react';
+
+type CSSType = {
+    syntax: string
+    bg: string
+    ui: string
+}
+
+type ThemeContextType = {
+    isLightTheme: boolean
+    light: CSSType
+    dark: CSSType
+    toggleTheme: () => void
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+    isLightTheme: true,
+    light: {
+        syntax: '#555',
+        bg: '#ddd',
+        ui: '#eee',
+    },
+    dark: {
+        syntax: '#ddd',
+        bg: '#333',
+        ui: '#555',
+    },
+    toggleTheme: () => {}
+})
+
+type ThemeContexProviderProps = {
+    children: React.ReactNode,
+}
+
+export default function ThemeContextProvider({children}: ThemeContexProviderProps) {
+    const [isLightTheme, setIsLightTheme] = useState<boolean>(true);
+
+    const toggleTheme = (): void => setIsLightTheme(!isLightTheme);
+    const light = {
+        syntax: '#555',
+        bg: '#ddd',
+        ui: '#eee',
+    }
+    const dark = {
+        syntax: '#ddd',
+        bg: '#333',
+        ui: '#555',
+    }
+
+    return (
+        <ThemeContext.Provider value={{light, dark, isLightTheme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
