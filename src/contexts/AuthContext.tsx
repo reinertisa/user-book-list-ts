d7f1bb9f676d049992d38ca5ba2e44403aedea62
Isa Reinert <reinert.isa@gmail.com>
2025-06-03 18:32:14 -0700

    feat(website): create AuthContext context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,27 @@
+import React, {createContext, useState} from 'react';
+
+type AuthContextType = {
+    isAuthenticated: boolean,
+    toggleAuth: () => void
+}
+
+export const AuthContext = createContext<AuthContextType>({
+    isAuthenticated: false,
+    toggleAuth: (): void => {}
+});
+
+type AuthContextProviderProps = {
+    children: React.ReactNode
+}
+
+export default function AuthContextProvider({children}: AuthContextProviderProps) {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+    const toggleAuth = (): void => setIsAuthenticated(!isAuthenticated);
+
+    return (
+        <AuthContext.Provider value={{isAuthenticated, toggleAuth}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
