832eabda7f85e916500e6b0abe3263f6f199465b
Isa Reinert <reinert.isa@gmail.com>
2025-06-03 18:32:20 -0700

    feat(website): create BookContext context

---
+++ src/contexts/BookContext.tsx
@@ -0,0 +1,40 @@
+import React, {createContext, useState} from 'react';
+import {v4 as uuid} from 'uuid';
+
+type BookContextType = {
+    books: Book[]
+    addBook: (title: string, author: string) => void
+    removeBook: (id: string) => void
+}
+
+export const BookContext = createContext<BookContextType>({
+    books: [],
+    addBook: () => {},
+    removeBook: () => {}
+});
+
+
+type BookContextProviderProps = {
+    children: React.ReactNode
+}
+
+type Book = {
+    id?: string,
+    title: string,
+    author: string
+}
+
+export default function BookContextProvider({children}: BookContextProviderProps) {
+    const [books, setBooks] = useState<Book[]>([]);
+
+    const addBook = (title: string, author: string): void =>
+        setBooks([...books, {title, author, id: uuid()}]);
+
+    const removeBook = (id: string): void => setBooks(books.filter(book => book.id !== id));
+
+    return (
+        <BookContext.Provider value={{books, addBook, removeBook}}>
+            {children}
+        </BookContext.Provider>
+    )
+}
